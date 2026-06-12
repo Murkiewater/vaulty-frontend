f86ad583d5843edfac024dbaf2f9faf92718fea9
Murkiewater <kekys492@gmail.com>
2026-06-12 16:35:25 +0500

    fixed 404 click on categories error / minor UI fixes

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -40,8 +40,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     return (
         <div className="bg-white">
             <Container>
-                <Billboard data={category.billboard} />
-                <div className="px-4 sm:px-6 lg:px-8">
+                <div className="px-4 pt-10 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
                         <MobileFilters sizes={sizes} colors={colors} />
                         <div className="hidden lg:block">
@@ -56,7 +55,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                                 data={colors}
                             />
                         </div>
-                        <div className="mt-6 lg:col-span-4 lg:mt-0">
+                        <div className="mt-6 mb-10 lg:col-span-4 lg:mt-0">
                             {products.length === 0 && <NoResults />}
                             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                                 {products.map((item) => (
